c75e936ca6e0b3ea0e236ef8555a7d680e124f4c
Kim Jin Tae ( Jin ) <57176548+JinlyDev@users.noreply.github.com>
2022-04-28 01:28:28 +0900

    C# Chapter 1 and 2

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using static System.Console;
+
+
+//namespace chapter1and2
+//{
+//    class program
+//    {
+//        static void Main(string[] args)
+//        {
+//            if (args.Length == 0)
+//            {
+//                Console.WriteLine("사용법 : Hello.exe <이름>");
+//                return;
+//            }
+//            WriteLine("Hello, {0}!", args[0]);
+//        }
+//    }
+//}
+
+
+using System;
+using static System.Console;
+
+
+namespace Chapter1and2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+        }
+    }
+}
